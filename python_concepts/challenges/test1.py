500ed62db15d82c566fdff44ac6b29f6257aa60e
Sunny Chugh <sunnychugh@Sunnys-MacBook-Air.local>
2024-10-26 21:24:17 +0530

    ree is a github girl

---
+++ python_concepts/challenges/test1.py
@@ -0,0 +1,19 @@
+# Create a function which determines whether a number is even or odd
+# ask the number from user
+# Push the code you do to the git repo. Ree man would finally become a github woman🥺
+
+n = int(input("Enter a number: "))
+
+def evenodd(a):
+    if a%2==0:
+        print("number is even")
+    else:
+        print("number is odd")
+    
+    return a
+
+
+output = evenodd(n)
+
+print(output)
+
